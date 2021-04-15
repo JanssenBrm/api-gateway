3107bb020eeeb05f7f8ad356bde089050bfe18b7
janssenb <bram.janssen@vito.be>
2021-04-15 09:28:11 +0200

    Get token from request

--- creditcheck.js
+++ creditcheck.js
@@ -1,8 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
-const checkCredit = () => {
+const checkCredit = (auth) => {
     return new Promise((resolve, reject) => {
+        console.log("Checking credit with token", auth);
         setTimeout(() => {
             reject('No sufficient credits');
         }, 500);
@@ -13,7 +14,7 @@ const setupCreditCheck = (app, routes) => {
     routes.forEach(r => {
         if (r.creditCheck) {
             app.use(r.url, function(req, res, next) {
-                checkCredit().then(() => {
+                checkCredit(req.headers["authorization"]).then(() => {
                     next();
                 }).catch((error) => {
                     res.status(402).send({error});
